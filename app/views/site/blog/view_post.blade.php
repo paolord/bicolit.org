f3ae33e33d3a935af7e9eaf3f88385714d5d868e
Michael Louie Loria <michaellouieloria@gmail.com>
2013-11-28 15:55:01 +0800

    addthis

--- app/views/site/blog/view_post.blade.php
+++ app/views/site/blog/view_post.blade.php
@@ -35,6 +35,22 @@
 
 <h3>{{ $post->title }}</h3>
 
+<div class="row">
+	<div class="col-md-11">
+        <p>
+            <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+                <a class="addthis_button_facebook"></a>
+                <a class="addthis_button_twitter"></a>
+                <a class="addthis_button_google_plusone_share"></a>
+                <a class="addthis_button_pinterest_share"></a>
+                <a class="addthis_button_compact"></a>
+            </div>
+            <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=michaellouieloria"></script>
+        <p>
+    </div>
+</div>
+
 <p>{{ $post->content() }}</p>
 
 <div>
